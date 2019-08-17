955f20150f41198cbd24d09c62842066f9c6d0f8
Steve Torres <storres032@Stevens-MacBook-Pro.local>
2019-08-17 12:59:59 -0400

    made post to firebase backend with angualr HTTP request.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
@@ -14,10 +14,17 @@ export class AppComponent implements OnInit {
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
-    // Send Http request
+    /* 
+    -Send Http request
+    -In angular HTTP requests return Observables and so to make requests and receive responses we must subscribe to them.
+    -You don't need to unsubscribe from the observable since the response back will complete the process & its an observable that is 
+    provided by angular so it will manage the unsubscribing of the observable.
+    */
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        // adding the .json at the end is only a firebase requirement not a REST api requirement.
+        "https://ng-course-c2f2f.firebaseio.com/posts.json",
+        // this is the request body that we are passing onto/
         postData
       )
       .subscribe(responseData => {
